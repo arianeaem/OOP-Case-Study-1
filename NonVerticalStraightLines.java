377ff8a9af7120c78cd0fcd16490546779b4a733
jollijb <louisejavier3@gmail.com>
2025-03-19 15:27:53 +0800

    Update NonVerticalStraightLines.java

--- NonVerticalStraightLines.java
+++ NonVerticalStraightLines.java
@@ -28,6 +28,24 @@ public class NonVerticalStraightLines {
         
         System.out.println();
     }
+
+    public void get_pt_slope(double[] values) {
+        System.out.print("Enter the slope=> ");
+        values[0] = input.nextDouble();
+        
+        System.out.print("Enter the x-y coordinates of the point separated by a space=> ");
+        values[1] = input.nextDouble();
+        values[2] = input.nextDouble();
+        
+        System.out.println();
+    }
+
+    public void slope_intcpt_from2_pt(double[] points, double[] result) {
+        double m = (points[3] - points[1]) / (points[2] - points[0]);
+        double b = points[1] - (m * points[0]);
+        result[0] = m;
+        result[1] = b;
+    }
     
     public static void main(String[] args) {
         NonVerticalStraightLines program = new NonVerticalStraightLines();
@@ -36,7 +54,12 @@ public class NonVerticalStraightLines {
         
         if (choice == 1) {
             double[] points = new double[4];
+            double[] result = new double[2];
             program.get2_pt(points);
+            program.slope_intcpt_from2_pt(points, result);
+        } else if (choice == 2) {
+            double[] values = new double[3];
+            program.get_pt_slope(values);
             // Rest of the program would go here
         }
     }
